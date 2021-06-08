f5dbb2ec5784412fd15b6b8b2e8ea84a10743e2e
Paul <suaup@hotmail.fr>
2021-06-08 11:39:42 +0200

    Argument parsing is clean (using getopts)

--- main.sh
+++ main.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 formatedMovieName outputFile" >&2
-    exit 1
+function usage {
+        echo "Usage: $(basename $0) [-no]" 2>&1
+        echo '   -n formattedName   to set the movie formated name'
+        echo '   -o outputPath      to set the output file'
+        exit 1
+}
+
+# Argument parsing & checking
+if [[ $# -eq 0 ]]; then
+   usage
 fi
 
-videoUrlOrError=`node main.js $1`
+while getopts "n:o:" opt; do
+    case $opt in
+        n) NAME="$OPTARG" ;;
+        o) OUTPUT_OPTION="-O $OPTARG" ;;
+        \?)
+            echo "Invalid option: -${OPTARG}."
+            usage
+            ;;
+    esac
+done
 
-if [ $? ]; then
+# The script is linked so the ressources around it should be taken with absolute path
+# (Cause the link isn't near them)
+baseDir=$(dirname $(realpath $0))
+
+# Getting url
+videoUrlOrError=`node ${baseDir}/main.js $NAME`
+
+# Checking movie existance
+if [ $? -eq 1 ]; then
     echo $videoUrlOrError
     exit $?
 fi
 
-wget $videoUrl -O $2
+# Downloading it
+wget $videoUrlOrError $OUTPUT_OPTION
 
 
