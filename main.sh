3990f4fd95d6dc06fc914aee9a6d385a4e771c06
Kat <suaup@hotmail.fr>
2021-08-08 14:12:16 +0200

    Parallel download first gets all the urls sequentially to keep all connections towards finding the url and try to prevent timeouts errors

--- main.sh
+++ main.sh
@@ -8,24 +8,27 @@ BASE_DIR=$(dirname $(realpath $0))
 VIDEO_URL_GETTER_SCRIPT_PATH="${BASE_DIR}/main.js"
 
 
-function handle_ressource {
-    url="$1"
+function usage {
+        echo "Usage: $(basename $0) [OPTIONS] FORMATTED-NAME..." 2>&1
+        echo '  -s                  FORMATTED-NAME(s) refers to a serie episode'
+        echo '  -p                  parallel mode (downloading part only)'
+        echo '  -o                  output name: 
+                                        %index -> index in args
+                                        %episode -> episode number (-s only)
+                                        %season -> season number (-s only)'
+        echo '  -d                  debug mode (headless=false)'
+        exit 1
+}
+
+function download_ressource {
+    ressource_url="$1"
     output_filename="$2"
 
-    contentUrlOrError=`node $VIDEO_URL_GETTER_SCRIPT_PATH $url $URL_GETTER_IS_HEADLESS 2> /dev/null`
-    if [[ $? -ne 0 ]]; then
-        echo $contentUrlOrError
-    else
-        if [[ ! "$OUTPUT_FORMAT" ]]; then
-            echo $contentUrlOrError
-        else
-            if ! wget $contentUrlOrError -c -O $output_filename; then
-                echo "Download of $output_filename failed"
-                return -1
-            fi
-        fi
+    if ! wget "$ressource_url" -c -O $output_filename; then
+        echo "Download of $output_filename failed"
+        return -1
     fi
-
+    
     return 0
 }
 
@@ -36,18 +39,6 @@ function get_episode_nb {
     echo "$1" | sed -n 's/.\+\/\([[:digit:]]\+\)-\([[:digit:]]\+\)/\2/p'
 }
 
-function usage {
-        echo "Usage: $(basename $0) [OPTIONS] FORMATTED-NAME..." 2>&1
-        echo '  -s                  FORMATTED-NAME(s) refers to a serie episode'
-        echo '  -p                  parallel mode'
-        echo '  -o                  output name: 
-                                        %index -> index in args
-                                        %episode -> episode number (-s only)
-                                        %season -> season number (-s only)'
-        echo '  -d                  debug mode (headless=false)'
-        exit 1
-}
-
 function makeUrl {
     if [[ "$2" -eq 0 ]]; then
         echo "https://123moviesplayer.com/movie/$1?src=mirror2"
@@ -55,6 +46,9 @@ function makeUrl {
         echo "https://123moviesplayer.com/show/$1?src=mirror2"
     fi
 }
+
+
+
 
 # Argument parsing & checking
 if [[ $# -eq 0 ]]; then
@@ -82,28 +76,74 @@ done
 
 shift $((OPTIND-1))
 
+
+
+function get_output_filename {
+    ressource_locator="$1"
+    index="$2"
+
+    if [[ "$OUTPUT_FORMAT" ]]; then
+        output_filename="$OUTPUT_FORMAT"
+        # Setting the variables
+        output_filename=`echo "$output_filename" | sed -e "s/%index/$index/g"`
+        if [[ "$IS_SERIE" -eq 1 ]]; then
+            season_nb=`get_season_nb $ressource_locator`
+            episode_nb=`get_episode_nb $ressource_locator`
+            output_filename=`echo "$output_filename" | sed -e "s/%season/$season_nb/g"`
+            output_filename=`echo "$output_filename" | sed -e "s/%episode/$episode_nb/g"`
+        fi
+    else
+        output_filename=""
+    fi
+
+    echo "$output_filename"
+}
+
+function handle_ressource {
+    ressource_locator="$1"
+    ressource_url="$2"
+    index=$3
+
+    if [[ ! "$OUTPUT_FORMAT" ]]; then
+        echo "$ressource_url"
+    else
+        output_filename=`get_output_filename "$ressource_locator" $index`
+        if [[ $IS_PARALLEL -eq 1 ]]; then
+            download_ressource "$ressource_url" "$output_filename" &
+        else
+            download_ressource "$ressource_url" "$output_filename"
+        fi
+    fi
+}
+
+
+declare -a ressources_urls
+declare -a ressources_locators 
 i=1
 while test $# -gt 0; do
-    url=`makeUrl $1 $IS_SERIE`
-    output_filename=`echo "$OUTPUT_FORMAT"`
+    url=`makeUrl "$1" $IS_SERIE`
 
-    # Setting the variables
-    output_filename=`echo "$output_filename" | sed -e "s/%index/$i/g"`
-    if [[ "$IS_SERIE" -eq 1 ]]; then
-        season_nb=`get_season_nb $1`
-        episode_nb=`get_episode_nb $1`
-        output_filename=`echo "$output_filename" | sed -e "s/%season/$season_nb/g"`
-        output_filename=`echo "$output_filename" | sed -e "s/%episode/$episode_nb/g"`
+    ressources_locators[$i]="$1"
+    ressources_urls[$i]=`node $VIDEO_URL_GETTER_SCRIPT_PATH $url $URL_GETTER_IS_HEADLESS 2> /dev/null`
+    if [[ $? -ne 0 ]]; then
+        # TODO: handle errors
+        echo ERROR
     fi
 
-    if [[ "$IS_PARALLEL" -eq 1 ]]; then
-        handle_ressource "$url" "$output_filename" &
-    else
-        handle_ressource "$url" "$output_filename"
+    if [[ $IS_PARALLEL -eq 0 ]]; then
+        handle_ressource "${ressources_locators[$i]}" "${ressources_urls[$i]}" $i
     fi
-
+    
+    i=$(( $i+1 ))
     shift
-    i=$((i+1))
 done
 
-wait
+
+if [[ $IS_PARALLEL -eq 1 ]]; then
+    for i in "${!ressources_locators[@]}"; do
+        handle_ressource "${ressources_locators[$i]}" "${ressources_urls[$i]}" $i
+    done
+
+    wait
+fi
+
